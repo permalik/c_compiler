fddd236d3e11760368f1a5ee2385de1447527e5e
permalik <permalik@protonmail.com>
2025-05-27 21:04:37 -0500

    feat: read c test file

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,35 @@
 pub fn main() !void {
-    std.debug.print("starting..\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const file = try fs.cwd().openFile("src/test.c", .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const reader = buf_reader.reader();
+
+    var line = std.ArrayList(u8).init(allocator);
+    defer line.deinit();
+
+    const writer = line.writer();
+    var line_no: usize = 0;
+    while (reader.streamUntilDelimiter(writer, '\n', null)) {
+        defer line.clearRetainingCapacity();
+        line_no += 1;
+
+        print("{d}:: {s}\n", .{ line_no, line.items });
+    } else |err| switch (err) {
+        error.EndOfStream => {
+            if (line.items.len > 0) {
+                line_no += 1;
+                print("{d}:: {s}\n", .{ line_no, line.items });
+            }
+        },
+        else => return err,
+    }
+
+    print("Line Count:: {d}\n", .{line_no});
 }
 
 // test "simple test" {
@@ -21,3 +51,5 @@ pub fn main() !void {
 // }
 
 const std = @import("std");
+const fs = std.fs;
+const print = std.debug.print;
